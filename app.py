131d20e75f0b9338d096b70808e7579fed901fd3
alben23 <alben21@gmail.com>
2021-10-21 17:00:52 -0300

    Se agrega validacion de JSON y mensajes de error

--- app.py
+++ app.py
@@ -36,15 +36,43 @@ def base():
 
 @app.route('/addAnswer',methods=['POST'])
 def answers_post():
-    data = request.json
-    res = MongoAPI("answers").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')
+    try:
+        if request.is_json:
+            data = request.json
+            print(data)
+            res = MongoAPI("answers").write(data)
+            status = 200
+        else:
+            print("Error JSON MAL FORMADO o mimetype incorrecto")
+            print(request.json)
+            res = "Error JSON MAL FORMADO o mimetype incorrecto"
+            status = 400
+    except:
+        res = "Error al procesar la peticion JSON MAL FORMADO o mimetype incorrecto"
+        status = 400
+    # data = request.json
+    # res = MongoAPI("polls").write(data)
+    return Response(response=json.dumps(res),status=status,mimetype='application/json') 
 
 @app.route('/addPoll',methods=['POST'])
 def polls_post():
-    data = request.json
-    res = MongoAPI("polls").write(data)
-    return Response(response=json.dumps(res),status=200,mimetype='application/json')    
+    try:
+        if request.is_json:
+            data = request.json
+            print(data)
+            res = MongoAPI("polls").write(data)
+            status = 200
+        else:
+            print("Error JSON MAL FORMADO o mimetype incorrecto")
+            print(request.json)
+            res = "Error JSON MAL FORMADO o mimetype incorrecto"
+            status = 400
+    except:
+        res = "Error al procesar la peticion JSON MAL FORMADO o mimetype incorrecto"
+        status = 400
+    # data = request.json
+    # res = MongoAPI("polls").write(data)
+    return Response(response=json.dumps(res),status=status,mimetype='application/json')     
 
 @app.route('/getPolls')
 def polls_get():
